b133c19e6960ad07d85478e5974d316c5da7bed3
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2018-10-15 14:23:35 +0530

    app structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth.shared/login/login.component';
+import { SignupComponent } from './auth.shared/signup/signup.component';
+import { HeaderComponent } from './layout.shared/header/header.component';
+import { FooterComponent } from './layout.shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingComponent } from './setting/setting.component';
+import { NewArticleComponent } from './new-article/new-article.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ArticleComponent,
+    ProfileComponent,
+    SettingComponent,
+    NewArticleComponent,
   ],
   imports: [
     BrowserModule
